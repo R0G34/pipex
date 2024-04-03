168c3db0cc7b2c4c2666720e5c827d544bafa08c
Albert Bausa Vozniy <abausa-v@c2r5s6.42madrid.com>
2024-04-03 17:26:21 +0200

    executing tests

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: abausa-v <abausa-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 19:17:25 by abausa-v          #+#    #+#             */
-/*   Updated: 2024/03/06 19:17:26 by abausa-v         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:21:00 by abausa-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,3 @@ void	*ft_bzero(void *dest, size_t count)
 	}
 	return (destiny);
 }
-/**
- * The function `ft_bzero` sets a block of memory to zero.
- * 
- * @param dest A pointer to the memory block that needs to be zeroed out.
- * @param count The parameter "count" represents the number of bytes
- * to be set to zero in the memory
- * block pointed to by "dest".
- * 
- * @return a pointer to the destination memory block that has been zeroed out.
- */
