f68eb95d841f1db9c57f13a44ddededd93deb108
R0G34 <quartalbert@gmail.com>
2024-08-11 17:59:21 +0200

    not finished yet

--- Makefile
+++ Makefile
@@ -1,55 +1,50 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: abausa-v <abausa-v@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/06/21 18:57:50 by abausa-v          #+#    #+#              #
-#    Updated: 2024/06/21 18:58:38 by abausa-v         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+CC		= gcc
+CFLAGS	= -Werror -Wextra -Wall
+NAME	= pipex
 
-NAME = pipex
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror
+SRC_PATH = src/
+OBJ_PATH = obj/
 
-LIBFT_DIR = libft
-LIBFT_FLAGS = -lft -L $(LIBFT_DIR)
-INCLUDES_FLAG = -Iincludes -I$(LIBFT_DIR)
+SRC	= pipex.c \
+	env_parsing.c \
+	utils.c \
+	init.c \
+	file_handler.c \
+	ft_putchar_fd.c \
+	ft_putendl_fd.c \
+	ft_putstr_fd.c \
+	ft_split.c \
+	ft_strjoin.c \
+	ft_strlen.c \
+	ft_strncmp.c \
+	ft_strnstr.c \
+	ft_substr.c \
+	ft_strlcpy.c \
+	get_next_line.c \
+	get_next_line_utils.c
+SRCS	= $(addprefix $(SRC_PATH), $(SRC))
+OBJ		= $(SRC:.c=.o)
+OBJS	= $(addprefix $(OBJ_PATH), $(OBJ))
 
-SRCS = sources/main.c sources/pipex.c sources/garbage_collector.c
-OBJS = $(SRCS:soures/%.c=objs/%.o)
-
-MKDIR = mkdir -p
-RM = rm -rf
-
-RED = "\\033[31m"
-RESET = "\\033[0m"
+INC		= -I ./includes/
 
 all: $(NAME)
 
-$(NAME): $(OBJS)
-	@echo $(RED) " - Compiling" $(RESET)
-	@for i in 1 2 3 4; do echo -n " .. "; sleep 1; done
-	@echo
-	@$(MAKE) -s -C $(LIBFT_DIR)
-	@$(CC) $(CFLAGS) $(OBJS) $(LIBFT_FLAGS) -o $(NAME)
-	@echo $(RED) " - Done!" $(RESET)
+$(OBJ_PATH)%.o: $(SRC_PATH)%.c
+	@mkdir -p $(OBJ_PATH) $(OBJ_PATH)get_next_line/
+	$(CC) $(CFLAGS) -c $< -o $@ $(INC)
 
-objs/%.o: soures/%.c
-	@$(MKDIR) objs
-	@$(CC) $(CFLAGS) $(INCLUDES_FLAG) -c $< -o $@
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $@
+
+bonus: all
 
 clean:
-	@$(RM) objs
-	@echo $(RED) " - Cleaned!" $(RESET)
+	rm -rf $(OBJ_PATH)
 
 fclean: clean
-	@$(RM) $(NAME)
-	@$(MAKE) -s -C $(LIBFT_DIR) fclean
-	@echo $(RED) " - Full Cleaned!" $(RESET)
+	rm -f $(NAME)
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all re clean fclean
--- includes/pipex.h
+++ includes/pipex.h
@@ -3,35 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abausa-v <abausa-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/21 17:38:45 by abausa-v          #+#    #+#             */
-/*   Updated: 2024/06/21 19:08:42 by abausa-v         ###   ########.fr       */
+/*   Created: 2022/03/29 18:22:36 by mcombeau          #+#    #+#             */
+/*   Updated: 2022/04/24 14:07:39 by mcombeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/libft.h"
-# include <stdbool.h>
-# include <sys/wait.h>
-# include <errno.h>
-# include <stdlib.h>
 # include <fcntl.h>
-# include <unistd.h>
+# include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <string.h>
+# include <errno.h>
+# include "get_next_line.h"
 
-typedef struct s_fd
+/*  Data    */
+typedef struct s_data
 {
-	int	file_in;
-	int	file_out;
-}	t_fd;
+	char	**envp;
+	char	**av;
+	int		ac;
+	int		heredoc;
+	int		fd_in;
+	int		fd_out;
+	int		*pipe;
+	int		nb_cmds;
+	int		child;
+	int		*pids;
+	char	**cmd_options;
+	char	*cmd_path;
+}		t_data;
 
-int		**open_pipes(t_fd *fds, int argc);
-char	**envp_parsing(char **envp);
-void	do_execve(int i, char **cmd, char **paths, char **envp);
-void	my_exec(char **cmd, char **envp);
-bool	pipex(int **pipes, int argc, char **argv, char **envp);
+/*  initialization.c    */
+t_data	init(int ac, char **av, char **envp);
+
+/*  env_parsing.c   */
+char	*get_cmd(char *cmd, t_data *data);
+
+/*  utils.c */
+void	exit_error(int error_status, t_data *data);
+int		msg(char *str1, char *str2, char *str3, int erno);
+void	close_fds(t_data *data);
+void	free_strs(char *str, char **strs);
+
+/*	ft functions */
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+/* file_handler.c */
+void	get_input_file(t_data *data);
+void	get_output_file(t_data *data);
+void	get_heredoc(t_data *data);
 
 #endif
